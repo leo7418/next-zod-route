726157b14a1ceff96553f4765c824734f89da16e
melvynxdev <melvynmal@gmail.com>
2025-05-20 18:36:11 +0800

    add route handler builder

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { createZodRoute } from './createZodRoute';
+export { type RouteHandlerBuilder } from './routeHandlerBuilder';
 export { type HandlerFunction, type RouteHandlerBuilderConfig } from './types';
